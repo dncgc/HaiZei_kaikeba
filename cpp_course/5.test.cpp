3d80cb037102b696dc9b553c4cb7e45e439abddd
dncgc <c1234567892017@outlook.com>
2020-12-23 22:44:41 +0800

    20-12-23 backup

---
+++ cpp_course/5.test.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: 5.test.cpp
+	> Author: Cui Guochong
+	> Mail: gc_cui@seu.edu.cn
+	> Created Time: Wed 23 Dec 2020 10:36:51 PM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
